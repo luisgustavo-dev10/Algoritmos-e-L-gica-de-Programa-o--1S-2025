3c5379353dafaf1716bb79195bd646695222f716
Luis Gustavo Almeida Andrade <luisgutomob2@gmail.com>
2025-03-29 23:06:05 -0300

    Add files via upload

---
+++ atividade2/triangulo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+	float altura, base;
+	float area_do_triangulo;
+	printf("A altura do triangulo eh: ");
+	scanf("%f", &altura);
+	printf("A base do triangulo eh: ");
+	scanf("%f", &base);
+	
+	area_do_triangulo = (altura * base / 2);
+	printf("A area_do_triangulo eh: %.2f", area_do_triangulo);
+	
+	system("pause");
+}
