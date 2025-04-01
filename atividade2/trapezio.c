c8d375675931e70b00864b651455998c5ac7650a
Luis Gustavo Almeida Andrade <luisgutomob2@gmail.com>
2025-03-31 21:45:38 -0300

    Add files via upload

---
+++ atividade2/trapezio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+	float base_maior, base_menor, altura;
+	float area_do_trapezio;
+	printf("A base_maior eh: ");
+	scanf("%f",&base_maior);
+	printf("A base_menor eh: ");
+	scanf("%f",&base_menor);
+	printf("a altura eh: ");
+	scanf("%f",&altura);
+	
+	area_do_trapezio = (base_maior + base_menor) * altura/2;
+	printf("A area_do_trapezio eh: %.2f",area_do_trapezio);
+	
+	system("pause");
+}
