aaba1de92fe41c96769a32cc7621db4faf34692f
Luis Gustavo Almeida Andrade <luisgutomob2@gmail.com>
2025-03-31 21:57:20 -0300

    Add files via upload

---
+++ atividade2/losango.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+	float diagonal_maior, diagonal_menor;
+	float area_do_losango;
+	printf("A diagonal_maior eh: ");
+	scanf("%f",&diagonal_maior);
+	printf("A diagonal_menor eh:  ");
+	scanf("%f",&diagonal_menor);
+	
+	area_do_losango = (diagonal_maior * diagonal_menor / 2);
+	printf("A area_do_losango eh: %.2f", area_do_losango);
+	
+	system("pause");
+	
+}
+	
