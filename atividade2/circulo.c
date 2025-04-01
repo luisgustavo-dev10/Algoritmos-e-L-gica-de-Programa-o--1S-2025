c9c7bdd95b633ce900f95df62a48994bd21abd33
Luis Gustavo Almeida Andrade <luisgutomob2@gmail.com>
2025-03-31 22:21:49 -0300

    Add files via upload

---
+++ atividade2/circulo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+	printf("A area_do_circulo = pi * raio ao quadrado \n");
+	float raio;
+	float area_do_circulo;
+	printf("O raio eh: ");
+	scanf("%f",&raio);
+	 
+	area_do_circulo = 3.14 * (raio*raio);
+	printf("A area_do_circulo eh : %.2f",area_do_circulo);
+	
+	system("pause");
+}
