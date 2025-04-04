64b4914bc18fdb2803fd4051d8e241bd53b56b57
luisgustavo-dev10 <luisgutomob2@gmail.com>
2025-04-04 12:45:36 -0300

    add atividade3

---
+++ atividade3/assistente.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+int main(int argc, char* argvv[]){
+    int opcao;
+    
+
+    printf("Assistente virtual de filmes\n");
+    system("pause");
+    printf("Escolher tipo de filme: Intenso ou Leve\n");
+    System("pause");
+    printf("1 - Filmes leves\n");
+    prinff("2 - Filmes intensos\n");
+    system("pause");
+    scanf("%d", &opcao);
+
+    if (opcao == 1);
+    {
+      printf("1 - para Comedia\n");
+      printf("2 - para animacao\n");
+      system("pause");
+      if (opcao == 1);
+      printf("Assistir: as Branquelas");
+      if (opcao == 2);
+      { printf("Assistir: Toy_Store");
+      }
+
+    }
+    if (opcao == 2);
+    {
+        prinf("1 - terror");
+        printf("2 - acao");
+        system("pause");
+        if (opcao == 1);
+        {
+            printf("1- para terror psicologico");
+            printf("2- para terror sobrenatural");
+            }
+            if (opcao == 1);{
+                printf("Assistir ao filme: Corra!")
+            }
+             if (opcao == 2){
+                printf("Assistir ao filme: Invocação_do_mal");
+             }
+        if ( opcao == 2){printf("1- para filmes de super-heroi");
+          printf("2 - para filmes de aventura_realista");}
+        {if (opcao == 1){
+
+          }
+
+          {
+            /* code */
+          }
+          
+            /* code */
+        }
+            
+        }   
+            
+        
+    }
+    
+
+    
+
+
+
+    
+    
+    return 0;
+}
